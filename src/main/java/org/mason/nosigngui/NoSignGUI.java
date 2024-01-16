44d80ea1859c98d81ccde9121332d672d9cced94
BuildTools <unconfigured@null.spigotmc.org>
2024-01-16 23:36:28 +1030

    Initial commit

---
+++ src/main/java/org/mason/nosigngui/NoSignGUI.java
@@ -0,0 +1,9 @@
+package org.mason.nosigngui;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NoSignGUI implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/org/mason/nosigngui/mixin/MixinClientPlayerEntity.java
@@ -0,0 +1,17 @@
+package org.mason.nosigngui.mixin;
+
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.block.entity.SignBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerEntity.class)
+public abstract class MixinClientPlayerEntity {
+
+    @Inject(method = "openEditSignScreen(Lnet/minecraft/block/entity/SignBlockEntity;Z)V", at = @At("HEAD"), cancellable = true)
+    private void onOpenEditSignScreen(SignBlockEntity sign, boolean front, CallbackInfo info) {
+        info.cancel();
+    }
+}
